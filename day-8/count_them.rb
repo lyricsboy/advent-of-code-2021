d58e5ea70d0865872136a655a7e5727b6564459f
Brian Hardy <lyricsboy@users.noreply.github.com>
2021-12-25 10:26:52 -0600

    Day 8 part 2 output

--- day-8/count_them.rb
+++ day-8/count_them.rb
@@ -2,10 +2,6 @@ require_relative "display_counter"
 
 counters = ARGF.each_line.to_a.map {|line| DisplayCounter.new(line) }
 
-decoded_output_values = counters.map(&:decoded_output_values)
-known_values = [1,4,7,8]
-known_value_count = decoded_output_values.reduce(0) do |sum, values|
-    sum += values.filter { |value| known_values.include?(value) }.count
-end
-
-puts "Found #{known_value_count} known values"
+decoded_output_values = counters.map(&:decoded_output_value)
+total_output = decoded_output_values.sum
+puts "Total output value: #{total_output}"
